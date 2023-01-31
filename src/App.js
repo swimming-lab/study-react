df3e98634bbceece308532158e68c87c734540ae
swimming <sooyoung209@naver.com>
2023-01-31 21:09:49 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Nav from './pages/Nav';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Nav = () => {
+	return (
+		<Link to="/">Home</Link>
+	);
+}
+
+export default Nav;
